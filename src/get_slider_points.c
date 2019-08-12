d5c91f3458b30c97dcc0d13912141d6a868cfb30
Gegel85 <Gegel85@laposte.net>
2019-08-12 21:22:35 +0200

    Fixed issue with same points

--- src/get_slider_points.c
+++ src/get_slider_points.c
@@ -4,6 +4,7 @@
 
 #include <stdlib.h>
 #include <osu_map_parser.h>
+#include <string.h>
 #include "slider_calcs.h"
 #include "frame_buffer.h"
 
@@ -12,6 +13,13 @@ void	getRealPointsSliders(OsuMap_hitObject *obj)
 	OsuMap_hitObjectSliderInfos	*infos = obj->additionalInfos;
 	OsuIntegerVectorArray		points = infos->curvePoints;
 
+	for (unsigned i = 0; i < points.length - 1; i++) {
+		if (points.content[i].x == points.content[i + 1].x && points.content[i].y == points.content[i + 1].y) {
+			memmove(&points.content[i], &points.content[i + 1], (points.length - i - 1) * sizeof(*points.content));
+			i--;
+			points.length--;
+		}
+	}
 	switch (infos->type) {
 		case 'B':
 			infos->curvePoints = getBezierPoints(points, obj->position);
@@ -23,10 +31,10 @@ void	getRealPointsSliders(OsuMap_hitObject *obj)
 			infos->curvePoints = getCirclePoints(points, obj->position);
 			break;
 		case 'C':
-			display_warning("Warning: Catmull sliders are deprecated and are, therefore, not supported");
+			display_warning("Warning: Catmull sliders are deprecated and are, therefore, not supported\n");
 			return;
 		default:
-			display_error("Invalid slider type");
+			display_error("Invalid slider type '%c'\n", infos->type);
 
 	}
 	free(points.content);
