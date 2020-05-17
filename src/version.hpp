978748d816212f71010803685c0071302e0afb50
Gegel85 <Gegel85@laposte.net>
2020-05-17 21:41:49 +0200

    Show parsed files infos

---
+++ src/version.hpp
@@ -0,0 +1,10 @@
+//
+// Created by Gegel85 on 17/05/2020.
+//
+
+#ifndef OSUREPLAY_PLAYER_VERSION_HPP
+#define OSUREPLAY_PLAYER_VERSION_HPP
+
+#define PLAYER_VERSION "0.0.1"
+
+#endif //OSUREPLAY_PLAYER_VERSION_HPP
